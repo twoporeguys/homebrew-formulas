95db62a455ca150dec5861aea6c5c18113355016
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2017-10-18 13:01:53 -0700

    ADDED: version info to librpc formula

--- Formula/librpc.rb
+++ Formula/librpc.rb
@@ -2,6 +2,7 @@ class Librpc < Formula
   desc "A general-purpose IPC/RPC library supporting asynchronous notifications, data streaming, exchange of file descriptors and WebSockets endpoint. Loosely based on Apple XPC interface."
   homepage "https://github.com/twoporeguys/librpc"
   url "https://github.com/twoporeguys/librpc/archive/master.tar.gz"
+  version "1.0"
 
   depends_on "cmake"
   depends_on "libsoup"
