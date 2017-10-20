939983b630b378a91d2d009703f3133de857ed4e
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2017-10-20 16:45:50 -0700

    UPDATED: actually librpc is not related to kivy at all

--- Formula/librpc.rb
+++ Formula/librpc.rb
@@ -6,11 +6,9 @@ class Librpc < Formula
 
   option "with-python", "Build with Python2 bindings"
   option "with-python3", "Build with Python3 binding"
-  option "with-kivy", "Build bindings for the Kivy"
 
   depends_on :python => :optional
   depends_on :python3 => :optional
-  depends_on "kivy" => :optional
 
   depends_on "cmake"
   depends_on "libsoup"
@@ -19,20 +17,9 @@ class Librpc < Formula
   depends_on "libusb"
   depends_on "libyaml"
 
-  depends_on "pkg-config" => "with-kivy" 
-  depends_on "sdl2" => "with-kivy" 
-  depends_on "sdl2_image" => "with-kivy" 
-  depends_on "sdl2_ttf" => "with-kivy" 
-  depends_on "sdl2_mixer" => "with-kivy" 
-  depends_on "gstreamer" => "with-kivy" 
-
   def install
     if build.with?("python") && build.with?("python3")
       odie "For now --with-python and --with-python3 options are mutually exclusive"
-    end 
-
-    if build.without?("python") && build.without?("python3") && build.with?("kivy")
-      odie "Cannot install for kivy if no --with-python nor --with-python3 option is selected"
     end 
 
     if build.with? "python"
@@ -49,11 +36,7 @@ class Librpc < Formula
       system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
       system "make", "install"
     end
-
-    if build.with? "kivy"
-      system "pip#{pyver}", "install", "--user", "kivy"
-    end
-
+    
     if build.without?("python") && build.without?("python3")
       system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}"
       system "make", "install"
