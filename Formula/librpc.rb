4274ebffba4ea457b104ab545f0413d2b9eba51b
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2017-10-20 13:27:50 -0700

    UPDATED: allow building  librpc with either python2, python3 or no python bindings at all

--- Formula/librpc.rb
+++ Formula/librpc.rb
@@ -4,19 +4,39 @@ class Librpc < Formula
   url "https://github.com/twoporeguys/librpc/archive/master.tar.gz"
   version "1.0"
 
+  option "with-python", "Build with python2 bindings"
+  option "with-python3", "Build with python3 binding"
+
+  depends_on :python => :optional
+  depends_on :python3 => :optional
+
   depends_on "cmake"
   depends_on "libsoup"
   depends_on "glib"
   depends_on "yajl"
   depends_on "libusb"
   depends_on "libyaml"
-  depends_on "python"
 
   def install
-    system "python2.7", "-m", "pip", "install", "--user", "Cython"
-    system "python2.7", "-m", "pip", "install", "--user", "enum34"
-    system "make", "PYTHON_VERSION=python2.7", "INSTALL_PREFIX=#{prefix}"
-    system "make", "install"
+    if build.with? "python"
+        pyver = Language::Python.major_minor_version "python2"
+        system "#{pyver}", "-m", "pip", "install", "--user", "Cython"
+	system "#{pyver}", "-m", "pip", "install", "--user", "enum34"
+	system "make", "PYTHON_VERSION=#{pyver}", "INSTALL_PREFIX=#{prefix}"
+	system "make", "install"
+    end
+
+    if build.with? "python3"
+        pyver = Language::Python.major_minor_version "python3"
+        system "#{pyver}", "-m", "pip", "install", "--user", "Cython"
+	system "make", "PYTHON_VERSION=#{pyver}", "INSTALL_PREFIX=#{prefix}"
+	system "make", "install"
+    end
+
+    if build.without? "python" && build.without? "python3" 
+	system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}"
+	system "make", "install"
+    end
   end
 
   test do
