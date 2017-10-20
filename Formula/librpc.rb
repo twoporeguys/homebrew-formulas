61a36dfa92a61bc606b867d8b4a82a9c5ce119f3
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2017-10-20 16:40:47 -0700

    ADDED: builds for kivy and error messages

--- Formula/librpc.rb
+++ Formula/librpc.rb
@@ -4,8 +4,9 @@ class Librpc < Formula
   url "https://github.com/twoporeguys/librpc/archive/master.tar.gz"
   version "1.0"
 
-  option "with-python", "Build with python2 bindings"
-  option "with-python3", "Build with python3 binding"
+  option "with-python", "Build with Python2 bindings"
+  option "with-python3", "Build with Python3 binding"
+  option "with-kivy", "Build bindings for the Kivy"
 
   depends_on :python => :optional
   depends_on :python3 => :optional
@@ -17,29 +18,48 @@ class Librpc < Formula
   depends_on "libusb"
   depends_on "libyaml"
 
+  depends_on "pkg-config" => "with-kivy" 
+  depends_on "sdl2" => "with-kivy" 
+  depends_on "sdl2_image" => "with-kivy" 
+  depends_on "sdl2_ttf" => "with-kivy" 
+  depends_on "sdl2_mixer" => "with-kivy" 
+  depends_on "gstreamer" => "with-kivy" 
+
   def install
+    if build.with?("python") && build.with?("python3")
+      odie "For now --with-python and --with-python3 options are mutually exclusive"
+    end 
+
+    if build.without?("python") && build.without?("python3") && build.with?("kivy")
+      odie "Cannot install for kivy if no --with-python nor --with-python3 option is selected"
+    end 
+
     if build.with? "python"
-        pyver = Language::Python.major_minor_version "python2"
-        system "pip#{pyver}", "install", "--user", "Cython"
-	system "pip#{pyver}", "install", "--user", "enum34"
-	system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
-	system "make", "install"
+      pyver = Language::Python.major_minor_version "python2"
+      system "pip#{pyver}", "install", "--user", "Cython==0.26.1"
+      system "pip#{pyver}", "install", "--user", "enum34"
+      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
+      system "make", "install"
     end
 
     if build.with? "python3"
-        pyver = Language::Python.major_minor_version "python3"
-        system "pip#{pyver}", "install", "--user", "Cython"
-	system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
-	system "make", "install"
+      pyver = Language::Python.major_minor_version "python3"
+      system "pip#{pyver}", "install", "--user", "Cython==0.26.1"
+      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
+      system "make", "install"
+    end
+
+    if build.with? "kivy"
+      system "pip#{pyver}", "install", "--user", "kivy"
     end
 
     if build.without?("python") && build.without?("python3")
-	system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}"
-	system "make", "install"
+      system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}"
+      system "make", "install"
     end
   end
 
   test do
-	  system "/usr/local/bin/python2.7", "-c", "'import librpc'"
+    system "/usr/local/bin/python2.7", "-c", "'import librpc'"
   end
 end
