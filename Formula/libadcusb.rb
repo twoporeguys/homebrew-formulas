20cafe717018f34cdc6f722a2e92b007034b813a
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2017-10-20 17:45:47 -0700

    ADDED: libadcusb recipe draft

---
+++ Formula/libadcusb.rb
@@ -0,0 +1,45 @@
+class Libadcusb < Formula
+  desc "ADC data transfer library"
+  homepage "https://github.com/twoporeguys/libadcusb"
+  url "https://github.com/twoporeguys/libadcusb/archive/master.tar.gz"
+  version "1.0"
+
+  option "with-python", "Build with Python2 bindings"
+  option "with-python3", "Build with Python3 binding"
+
+  depends_on :python => :optional
+  depends_on :python3 => :optional
+
+  depends_on "glib"
+  depends_on "libusb"
+  depends_on "numpy"
+
+  def install
+    if build.with?("python") && build.with?("python3")
+      odie "For now --with-python and --with-python3 options are mutually exclusive"
+    end 
+
+    if build.with? "python"
+      pyver = Language::Python.major_minor_version "python2"
+    end
+
+    if build.with? "python3"
+      pyver = Language::Python.major_minor_version "python3"
+    end
+
+    if build.with?("python") || build.with?("python3")
+      system "pip#{pyver}", "install", "--user", "Cython==0.26.1"
+      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
+      system "make", "install"
+    end
+    
+    if build.without?("python") && build.without?("python3")
+      system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "/usr/local/bin/python2.7", "-c", "'import librpc'"
+  end
+end
