ca3210acb96b5a223595ecf20f791596af4b653b
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2018-01-10 00:17:14 +0100

    UPDATED: bumped librpc to v0.3.2 + enabled libdispatch support in recipe by default

--- Formula/librpc.rb
+++ Formula/librpc.rb
@@ -1,8 +1,8 @@
 class Librpc < Formula
   desc "A general-purpose IPC/RPC library supporting asynchronous notifications, data streaming, exchange of file descriptors and WebSockets endpoint. Loosely based on Apple XPC interface."
   homepage "https://github.com/twoporeguys/librpc"
-  url "https://github.com/twoporeguys/librpc/archive/v0.3.1.tar.gz"
-  sha256 "e550cf1dd30c5492f4041eb2b4fb38adcec5f758df74d5526d7d2354b32eae13"
+  url "https://github.com/twoporeguys/librpc/archive/v0.3.2.tar.gz"
+  sha256 "9fdc9c8ce68351b32881c7c9491223ec7b1285eca1f7417ea739d3e916eb4e26"
 
   option "with-python", "Build with Python2 bindings"
   option "with-python3", "Build with Python3 binding"
@@ -26,19 +26,19 @@ class Librpc < Formula
       pyver = Language::Python.major_minor_version "python2"
       system "pip#{pyver}", "install", "--user", "Cython==0.26.1"
       system "pip#{pyver}", "install", "--user", "enum34"
-      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
+      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}", "ENABLE_LIBDISPATCH=ON"
       system "make", "install"
     end
 
     if build.with? "python3"
       pyver = Language::Python.major_minor_version "python3"
       system "pip#{pyver}", "install", "--user", "Cython==0.26.1"
-      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
+      system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}", "ENABLE_LIBDISPATCH=ON"
       system "make", "install"
     end
     
     if build.without?("python") && build.without?("python3")
-      system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}"
+      system "make", "BUILD_PYTHON=OFF", "INSTALL_PREFIX=#{prefix}", "ENABLE_LIBDISPATCH=ON"
       system "make", "install"
     end
   end
