d42db1e8deb7e7fd559e45763333f9a39270c7ec
Brett Gyarfas <bg@2pg.io>
2019-04-05 17:53:55 -0700

    Bump cython version to 0.29.6

--- Formula/libadcusb.rb
+++ Formula/libadcusb.rb
@@ -6,7 +6,7 @@ class Libadcusb < Formula
 
   option "with-python@2", "Build with Python2 bindings"
   option "with-python", "Build with Python3 binding"
- 
+
   depends_on "python@2" => :optional
   depends_on "python" => :optional
 
@@ -18,18 +18,18 @@ class Libadcusb < Formula
   def install
     if build.with?("python@2") && build.with?("python")
       odie "For now --with-python@2 and --with-python options are mutually exclusive"
-    end 
+    end
 
     if build.with? "python@2"
       pyver = Language::Python.major_minor_version "python2"
-      system "pip#{pyver}", "install", "--user", "Cython==0.26.1"
+      system "pip#{pyver}", "install", "-U", "--user", "Cython==0.29.6"
       system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
       system "make", "install"
     end
 
     if build.with? "python"
       pyver = Language::Python.major_minor_version "python3"
-      system "pip#{pyver}", "install", "--user", "Cython==0.28.4"
+      system "pip#{pyver}", "install", "-U", "--user", "Cython==0.29.6"
       system "make", "PYTHON_VERSION=python#{pyver}", "INSTALL_PREFIX=#{prefix}"
       system "make", "install"
     end
@@ -41,6 +41,6 @@ class Libadcusb < Formula
   end
 
   test do
-    system "/usr/local/bin/python2.7", "-c", "'import librpc'"
+    system "/usr/local/bin/python2.7", "-c", "'import adcusb'"
   end
 end
